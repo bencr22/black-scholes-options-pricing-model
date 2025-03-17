9d5636e4ad4cd5e58e1d96da9bff9781f0383454
Ben <bencruwys@gmail.com>
2025-03-17 21:24:12 +0000

    Improved formatting, readability, and reduced duplicated code.

--- main.py
+++ main.py
@@ -1,38 +1,54 @@
 from math import e, sqrt, log
 from scipy.stats import norm
 
-"""
-Call = S * N(d1) - N(d2) * K(e^-rT)
-Put = N(-d2) * K(e^-rT) - N(-d1) * S
-"""
 
+def calculate_adjusted_strike_price(strike_price, interest_rate, years_to_maturity) -> float:
+    # Calculate the current value of the strike price. 
+    # Value of money changes over time, so discount strike price to reflect.
+    return (strike_price * (e ** (-1 * interest_rate * years_to_maturity)))
+
+
+def calculate_d_values(current_price, strike_price, interest_rate, years_to_maturity, volatility) -> float:
+    d_denominator = volatility * sqrt(years_to_maturity)
+
+    d1_numerator = log(current_price / strike_price) + ((interest_rate + (volatility ** 2) / 2) * years_to_maturity)
+    d1 = d1_numerator / d_denominator
+
+    d2_numerator = log(current_price / strike_price) + ((interest_rate - (volatility ** 2) / 2) * years_to_maturity)
+    d2 = d2_numerator / d_denominator
+
+    return d1, d2
+
+
+def calculate_call_price(current_price, strike_price, interest_rate, years_to_maturity, volatility) -> float:
+    adjusted_strike_price = calculate_adjusted_strike_price(strike_price, interest_rate, years_to_maturity)
+    d1, d2 = calculate_d_values(current_price, strike_price, interest_rate, years_to_maturity, volatility)
+
+    #Call = S * N(d1) - N(d2) * K(e^-rT)
+    return (current_price * norm.cdf(d1)) - (norm.cdf(d2) * adjusted_strike_price)
+
+
+def calculate_put_price(current_price, strike_price, interest_rate, years_to_maturity, volatility) -> float:
+    adjusted_strike_price = calculate_adjusted_strike_price(strike_price, interest_rate, years_to_maturity)
+    d1, d2 = calculate_d_values(current_price, strike_price, interest_rate, years_to_maturity, volatility)
+
+    #Put = N(-d2) * K(e^-rT) - N(-d1) * S
+    return (norm.cdf(-d2) * adjusted_strike_price) - (norm.cdf(-d1) * current_price)
+
+
+# Current price of the stock.
 current_stock_price = 100
+# Price at which the option many be purchased for or sold at.
 strike_price = 95
-interest_rate = 0.035
+# Annual risk free interest rate. 
+interest_rate = 0.04
+# Years until the option expires.
 years_to_maturity = 4
+# Calcualted by taken the std. dev. of the log of returns.
 volatility = 0.4
 
-# Calculate the current value of the strike price. 
-# Money changes value over time, so discount the strike price to reflect that.
-strike_price_current_value = strike_price * (e ** (-1 * interest_rate * years_to_maturity))
-
-"""
-Call = stock_price * N(d1) - N(d2) * strike_price_current_value
-Put = N(-d2) * strike_price_current_value - N(-d1) * stock_price
-
-where, N(x) = integral( (e^((-x^2)/2)) / sqrt(2 * pi) ) between -inf and x.
-"""
-
-d_denominator = volatility * sqrt(years_to_maturity)
-
-d1_numerator = log(current_stock_price / strike_price) + ((interest_rate + (volatility ** 2) / 2) * years_to_maturity)
-d1 = d1_numerator / d_denominator
-
-d2_numerator = log(current_stock_price / strike_price) + ((interest_rate - (volatility ** 2) / 2) * years_to_maturity)
-d2 = d2_numerator / d_denominator
-
-call = (current_stock_price * norm.cdf(d1)) - (norm.cdf(d2) * strike_price_current_value)
-put = (norm.cdf(-d2) * strike_price_current_value) - (norm.cdf(-d1) * current_stock_price)
+call = calculate_call_price(current_stock_price, strike_price, interest_rate, years_to_maturity, volatility)
+put = calculate_put_price(current_stock_price, strike_price, interest_rate, years_to_maturity, volatility)
 
 print(f"Call: {call}")
 print(f"Put: {put}")
