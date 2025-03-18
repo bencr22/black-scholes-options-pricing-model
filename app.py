d048c34bbbe99f892b3c137a47ae26b263ade1aa
Ben <bencruwys@gmail.com>
2025-03-18 21:31:20 +0000

    Added Streamlit GUI.

--- app.py
+++ app.py
@@ -24,16 +24,12 @@ with inputs_col1:
     # Get inputs from user.
     asset_price = st.number_input("Current Asset Price", min_value=0.00, value=100.00, step=0.01)
     strike_price = st.number_input("Strike Price", min_value=0.00, value=100.00, step=0.01)
-    maturity_date = st.date_input("Maturity Date", min_value=tomorrow, value=one_year_fowards,  format="DD/MM/YYYY")
-
-    # Calculate number of years between maturity date and today.
-    years_until_maturity = (maturity_date - today).days / 365
+    years_until_maturity = st.number_input("Years Until Maturity", min_value=0.00, value=2.00, step=0.01)
 
 with inputs_col2:
     # Get inputs from user.
     interest_rate = st.number_input("Annual Risk-Free Interest Rate (%)", min_value=1.00, value=2.5, step=0.01)
     volatility = st.number_input("Volatility (%)", min_value=0.0, max_value=100.0, value=30.0, step=0.1)
-
 
 
 # Calculate call and put value.
